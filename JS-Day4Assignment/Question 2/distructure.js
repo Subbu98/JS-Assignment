951cce55549b7518b426ec41c266780947362a4d
Subbu98 <68160786+Subbu98@users.noreply.github.com>
2020-07-19 23:22:34 +0530

    Add files via upload

---
+++ JS-Day4Assignment/Question 2/distructure.js
@@ -0,0 +1,12 @@
+const student = {
+    name : "Helsinki",
+    age : 24,
+    projects : {
+        dicegame : "Two players dice game using JavaScript"
+       
+    } 
+}
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.projects.dicegame);
---
+++ JS-Day4Assignment/Question 3/shopping.js
@@ -0,0 +1,6 @@
+console.log("hi")
+
+const shoppingList = ["Vegetables","milk","fruits"];
+
+const shoppingBasket = [...shoppingList,"eggs","shirts","pants"];
+console.log(shoppingBasket)
